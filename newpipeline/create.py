00b7a98e12865ba2ac49f880b2c2370186af8fd5
Deepraj Das <ddasjio123@gmail.com>
2025-08-14 17:00:03 +0530

    Working on separate pipeline

---
+++ newpipeline/create.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+from glob import glob
+from tqdm import tqdm
+from time import time
+from scipy.io import savemat
+from multiprocessing import cpu_count, Pool
+from utils.functions import extractFeature
+
+# Manually set args
+args = argparse.Namespace(
+    dataset_dir="../datasets/CASIA1/*",
+    template_dir="./templates/CASIA1/",
+    number_cores=cpu_count()
+)
+
+# creating a pool function to use with multiprocessing
+def pool_func(file):
+    template, mask, _ = extractFeature(file, multiprocess=False)
+    basename = os.path.basename(file)
+    out_file = os.path.join(args.template_dir, "%s.mat" % (basename))
+    savemat(out_file, mdict={'template': template, 'mask': mask})
+
+# time it
+start = time()
+if not os.path.exists(args.template_dir):
+    print("makedirs", args.template_dir)
+    os.makedirs(args.template_dir)
+
+files = glob(os.path.join(args.dataset_dir, "*_1_*.jpg"))
+n_files = len(files)
+print("N# of files which we are extracting features", n_files)
+# extracting features using multiple cores (number_cores)
+pools = Pool(processes=args.number_cores)
+for _ in tqdm(pools.imap_unordered(pool_func, files), total=n_files):
+    pass
+# total time
+end = time()
+print('\nTotal time: {} [s]\n'.format(end-start))
